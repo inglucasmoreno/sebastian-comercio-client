701342e91fd6d8b618217ad82f00508063fae059
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2022-10-05 23:22:35 -0300

    Cuentas corrientes

---
+++ src/app/services/cc-clientes-movimientos.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CcClientesMovimientosService {
+
+  get getToken(): any {
+    return {
+      'Authorization': localStorage.getItem('token')
+    };
+  }
+
+  constructor(private http: HttpClient) {}
+
+  // Nuevo movimiento
+  nuevoMovimiento(data: any): Observable<any> {
+    return this.http.post(`${base_url}/cc-clientes-movimientos`, data, {
+      headers: this.getToken
+    });
+  };
+
+  // Movimiento por ID
+  getMovimiento(id: string): Observable<any> {
+    return this.http.get(`${base_url}/cc-clientes-movimientos/${ id }`,{ 
+      headers: this.getToken
+    });
+  };
+
+  // Listar movimientos
+  listarMovimientos(parametros?: any): Observable<any> {
+    return this.http.get(`${base_url}/cc-clientes-movimientos`, {
+      params: {
+        direccion: parametros?.direccion || 1,
+        columna: parametros?.columna || 'descripcion'
+      },
+      headers: this.getToken
+    });
+  }
+
+  // Actualizar movimientos
+  actualizarMovimientos(id:string, data: any): Observable<any> {
+    return this.http.put(`${base_url}/cc-clientes-movimientos/${id}`, data, {
+      headers: this.getToken
+    });
+  }  
+
+}
---
+++ src/app/services/cc-clientes.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CcClientesService {
+
+  get getToken(): any {
+    return {
+      'Authorization': localStorage.getItem('token')
+    };
+  }
+
+  constructor(private http: HttpClient) {}
+
+  // Nueva cuenta corriente
+  nuevaCuentaCorriente(data: any): Observable<any> {
+    return this.http.post(`${base_url}/cc-clientes`, data, {
+      headers: this.getToken
+    });
+  };
+
+  // Cuenta corriente por ID
+  getCuentaCorriente(id: string): Observable<any> {
+    return this.http.get(`${base_url}/cc-clientes/${ id }`,{ 
+      headers: this.getToken
+    });
+  };
+
+  // Listar cuentas corrientes
+  listarCuentasCorrientes(parametros?: any): Observable<any> {
+    return this.http.get(`${base_url}/cc-clientes`, {
+      params: {
+        direccion: parametros?.direccion || 1,
+        columna: parametros?.columna || 'descripcion'
+      },
+      headers: this.getToken
+    });
+  }
+
+  // Actualizar cuenta corriente
+  actualizarCuentaCorriente(id:string, data: any): Observable<any> {
+    return this.http.put(`${base_url}/cc-clientes/${id}`, data, {
+      headers: this.getToken
+    });
+  }  
+
+}
