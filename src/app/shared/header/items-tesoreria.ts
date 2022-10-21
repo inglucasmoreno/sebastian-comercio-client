14528e9ccf4ab86881334237e478a0e0da53002a
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2022-10-21 13:53:58 -0300

    Cambio de PC

--- src/app/shared/header/items-tesoreria.ts
+++ src/app/shared/header/items-tesoreria.ts
@@ -2,7 +2,6 @@
 // ITEMS PARA BARRA DE NAVEGACION - TESORERIA
 
 export const itemsTesoreria = [
-  { svg: 'cajas', route: 'cajas', item: 'Cajas', permiso:['TESORERIA_NAV'] },
   { svg: '', route: 'cc-proveedores', item: 'Cuenta corriente - Proveedores', permiso:['TESORERIA_NAV'] },
   { svg: '', route: 'cheques', item: 'Cheques', permiso:['TESORERIA_NAV'] },
   { svg: '', route: 'bancos', item: 'Bancos', permiso:['TESORERIA_NAV'] },
