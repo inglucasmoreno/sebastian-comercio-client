9e2a909b8040f94f58aef0c19d06e6cc39e9eef9
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2022-12-14 21:23:30 -0300

    Para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // base_url: 'http://159.89.84.242',
-  base_url: 'http://157.230.13.158'
+  base_url: 'http://159.89.84.242',
+  // base_url: 'http://157.230.13.158'
 };
