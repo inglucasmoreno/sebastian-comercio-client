787d357331e8ea228a0b58f98d81a81cfb31db6c
Moreno Lucas Omar <ing.lucasmoreno@gmail.com>
2023-05-07 22:16:59 -0300

    Para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // base_url: 'http://159.89.84.242',
+  base_url: 'http://159.89.84.242',
   // base_url: 'http://157.230.13.158'
-  base_url: 'https://equinocciotech.com'
+  // base_url: 'https://equinocciotech.com'
 };
